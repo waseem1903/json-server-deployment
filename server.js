ef28d7b362052306fc7664fa80c1ba86a42ead0c
waseemKhan123 <waseemKhan@gmail.com>
2023-02-09 11:29:11 +0500

    server changes

--- server.js
+++ server.js
@@ -5,12 +5,18 @@ const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
+server.use(jsonServer.bodyParser)
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
     '/blog/:resource/:id/show': '/:resource/:id'
 }))
 server.use(router)
+router.render = (req, res) => {
+    res.status(500).jsonp({
+        error: "error message here"
+    })
+}
 server.listen(3001, () => {
     console.log('JSON Server is running')
 })
